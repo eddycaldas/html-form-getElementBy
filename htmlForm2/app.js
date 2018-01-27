abcf89460c32849a1a693a62dd1fad6e6179f672
Eddy Caldas <eddycaldas@live.com>
2018-01-26 17:58:40 -0700

    forms and saved files

--- htmlForm2/app.js
+++ htmlForm2/app.js
@@ -14,18 +14,34 @@
 //   e.preventDefault();
 // }
 
-var inputForm = document.getElementById('inputForm');
-var textInput = document.getElementById('textInput');
-var submitButton = document.getElementById('submit');
+// var inputForm = document.getElementById('inputForm');
+// var textInput = document.getElementById('textInput');
+// var submitButton = document.getElementById('submit');
+// 
+// inputForm.addEventListener('submit', numbers);
+// function numbers(e){
+//   var inputToString = textInput.value.toString();
+//   var regEx = (/\d/g);
+//   var filtered = inputToString.match(regEx);
+//     // console.log(filtered);
+//     document.getElementById('outputText').innerHTML = filtered;
+// 
+// 
+//   e.preventDefault();
+// }
 
-inputForm.addEventListener('submit', numbers);
-function numbers(e){
-  var inputToString = textInput.value.toString();
+
+var formInput = document.getElementById('formInput');
+var inputText = document.getElementById('inputText');
+var submitButton = document.getElementById('submitButton');
+
+formInput.addEventListener('submit', getNumbers);
+function getNumbers(e){
+  var inputToString = inputText.value.toString();
   var regEx = (/\d/g);
-  var filtered = inputToString.match(regEx);
-    // console.log(filtered);
-    document.getElementById('outputText').innerHTML = filtered;
+  var onlyNumbers = inputToString.match(regEx)
   
+  document.getElementById('showNumbers').innerHTML = onlyNumbers;
   
   e.preventDefault();
 }
